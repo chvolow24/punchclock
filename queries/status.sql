19757b492ad87848e9e9d8411d53a34c6d9bba8b
Charlie Volow <charlie.volow@gmail.com>
2026-05-01 13:53:30 -0400

    rm comment in status.sql

--- queries/status.sql
+++ queries/status.sql
@@ -1,13 +1,3 @@
--- CREATE TABLE IF NOT EXISTS time_blocks(
--- 	block_id INTEGER PRIMARY KEY
---     , punch_in_ts TEXT
---     , punch_out_ts TEXT
---     , block_dur_seconds INTEGER
---     , job_id INTEGER
---     , backed_up_ts TEXT
---     , submitted_ts TEXT
--- );
-
 WITH tmp AS (
 	SELECT * FROM time_blocks
 	WHERE job_id = ? -- PARAM 0: job_id
